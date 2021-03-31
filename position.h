c8c948fd97023043c1969a4ed8482223654bcc30
Bruno <bga@gmail.com>
2021-03-31 07:36:05 -0700

    Make it completelly stateless.
    - Less code and also simplifies taking decisions on positions.
    - Remove the coordinates.* files and integrate code in   position.*.

--- position.h
+++ position.h
@@ -3,31 +3,20 @@
 
 #include <blinklib.h>
 
-#include "coordinates.h"
-
 namespace position {
 
-// Set up the local Blink position based on the given parameters. The
-// relative_local_face parameter is the relative face number of the face we got
-// the given remote_x and remote_y coordinates from so it needs to be sent from
-// one Blink to the other in some way (either using face values or datagrams).
-void Setup(byte relative_local_face, int8_t remote_x, int8_t remote_y);
+struct Coordinates {
+  int8_t x;
+  int8_t y;
+};
 
-// Resets the local Blink position to 0, 0, 0.
-void Reset();
+Coordinates Local(byte relative_remote_face, Coordinates remote);
 
-// Update position to the given x and y coordinates without changing the
-// orientation.
-void Update(int8_t x, int8_t y);
+Coordinates Remote(byte relative_local_face, Coordinates local);
 
-// Returns the coordinates for the local Blink.
-Coordinates Local();
+int8_t Z(Coordinates coordinates);
 
-// Returns the coordinates for the Blink connected to the given
-// relative_local_face.
-Coordinates Remote(byte relative_local_face);
-
-byte Distance(Coordinates coordinates);
+byte Distance(Coordinates coordinates1, Coordinates coordinates2);
 
 }  // namespace position
 
--- position.ino
+++ position.ino
@@ -3,15 +3,13 @@
 #include "position.h"
 #include "src/blinks-debug/debug.h"
 
-void setup() {
-  // Set ourselves as the origin. Assume our relative local faces and absolute
-  // local faces match.
-  position::Setup(0, 0, 0);
-}
+void setup() {}
+
+static position::Coordinates local_;  // Starts as the origin.
 
 void loop() {
   FOREACH_FACE(face) {
-    position::Coordinates remote_position = position::Remote(face);
+    position::Coordinates remote_position = position::Remote(face, local_);
     LOGF("Remote position for face ");
     LOG(face);
     LOGF(" is ");
